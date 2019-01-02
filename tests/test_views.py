b3d4a77654f3a499df060605b641cebab827ff78
kyakulumbye ahmad <kyakusahmed@outlook.com>
2019-01-03 02:55:20 +0300

    add temporary challenge 3

--- tests/test_views.py
+++ tests/test_views.py
@@ -2,8 +2,99 @@ import json
 from .base_test import BaseTest
 
 
-class IncidentTest(BaseTest):
+class UserTest(BaseTest):
 
+
+    def test_user_register(self):
+        """Test successful register"""
+        regiseter_user = {
+            "first_name":"ahmed",
+            "last_name":"kyakus",
+            "email": "kyakusahmed@out.com",
+            "password": "1988ch",
+            "isAdmin": False
+        }
+        response = self.app.post('/api/v1/users/register', json=regiseter_user)
+        self.assertEqual(response.status_code, 201)
+        assert json.loads(response.data)['data'] == "user registered successfully"
+
+    def test_missing_fields(self):
+        """Test register without data"""
+        regiseter_user = {
+            "first_name":"",
+            "last_name":"kyakus",
+            "email": "kyakusahmed@outlook.com",
+            "password": "1988ch",
+            "isAdmin": False
+        }
+        response = self.app.post('/api/v1/users/register', json=regiseter_user)
+        self.assertEqual(response.status_code, 400)
+        assert json.loads(response.data)['error']['message'] == 'first_name is required'
+
+    def test_user_register_email_exist(self):
+        """Test user email exists."""
+        register_user = {
+            "first_name":"avdb",
+            "last_name":"kyakus",
+            "email": "kyakusahmed@outlook.com",
+            "password": "1988ch",
+            "isAdmin": False
+        }
+        self.app.post('/api/v1/users/register', json=register_user)
+        response = self.app.post('/api/v1/users/register', json=register_user)
+        self.assertEqual(response.status_code, 200)
+        assert json.loads(response.data)['message'] == "user registered already"
+
+    def test_successful_user_login(self):
+        """Test successful login."""
+        register_user = {
+            "first_name":"avdb",
+            "last_name":"kyakus",
+            "email": "kyakusahmed@outlook.com",
+            "password": "1988ch",
+            "isAdmin": False
+        }
+        self.app.post('/api/v1/users/register', json=register_user)
+        response = self.app.post('/api/v1/users/login', json=register_user)
+        self.assertEqual(response.status_code, 200)
+        assert json.loads(response.data)['message'] == "Login successful"
+
+    def test_user_not_registered(self):
+        """Test missing login email and password."""
+        register_user = {
+            "email": "kyaku@outlook.com",
+            "password": "1988fh"
+        }
+        response = self.app.post('/api/v1/users/login', json=register_user)
+        self.assertEqual(response.status_code, 200)
+        assert json.loads(response.data)['message'] == "register first"
+
+    def test_invalid_email(self):
+        register_user = {
+            "email": "kyaku@outlook.",
+            "password": "1988fh"
+        }
+        response = self.app.post('/api/v1/users/login', json=register_user)
+        self.assertEqual(response.status_code, 400)
+        assert json.loads(response.data)['error']['message'] == "invalid email"
+
+    
+    def test_password_too_short(self):
+        register_user = {
+            "first_name":"avdb",
+            "last_name":"kyakus",
+            "email": "kyaku@outlook.com",
+            "password": "18fh",
+            "isAdmin": False
+        }
+        response = self.app.post('/api/v1/users/register', json=register_user)
+        self.assertEqual(response.status_code, 400)
+        assert json.loads(response.data)['error'] == "password should be atleast five characters"
+
+
+
+class IncidentTest(BaseTest):
+    
 
     def test_add_incident(self):
         """Test successful register"""
@@ -244,7 +335,87 @@ class IncidentTest(BaseTest):
 
 
 
+class InterventionsTest(BaseTest):
+    
+    
+    def test_add_intervention(self):
+        """Test intervention is added successfully"""
+        token = self.return_user_token()
+        incident_Test = {
+            "description": "w3y45u w4u5ik 5w5i63",
+            "image": "",
+            "location": "setjyk",
+            "video": "",
+            "status": "pending"
+        }
+        response = self.app.post('/api/v1/incidents', headers={"Authorization": "Bearer " + token}, json=incident_Test)
+        test_data = {
+            "redflag_id": 1,
+            "comment": "rctyhvubinoui",
+            "inter_location": "0.5, 32.5"
+        }
+        response = self.app.post('/api/v1/interventions', 
+        headers={"Authorization": "Bearer " + token}, json=test_data)
+        self.assertEqual(response.status_code, 201)
+        assert json.loads(response.data)['message'] == "intervention added successfully"
+        assert json.loads(response.data)['status'] == 201
+
+
+    def test_add_inyervention_redflag_id_doesnot_exist(self):
+        """Test with redflag_id which doesnot exist"""
+        token = self.return_user_token()
+        test_data = {
+            "redflag_id": 10,
+            "comment": "rctyhvubinoui",
+            "inter_location": "0.5, 32.5"
+        }
+        response = self.app.post('/api/v1/interventions', 
+        headers={"Authorization": "Bearer " + token}, json=test_data)
+        self.assertEqual(response.status_code, 201)
+        assert json.loads(response.data)['message'] == "user_id or redflag_id doesnot exist , intervention not created"
+        assert json.loads(response.data)['status'] == 201
     
 
 
+    def test_comment_is_missing(self):
+        """Test comment field is missing"""
+        token = self.return_user_token()
+        test_data = {
+            "redflag_id": 10,
+            "comment": "",
+            "inter_location": "0.5, 32.5"
+        }
+        response = self.app.post('/api/v1/interventions', headers={"Authorization": "Bearer " + token}, json=test_data)
+        self.assertEqual(response.status_code, 400)
+        assert json.loads(response.data)['error']['message'] == "comment is required"
+        assert json.loads(response.data)['error']['field'] == "comment"
+        assert json.loads(response.data)['status'] == 400
 
+
+    def test_add_intervention_with_out_token(self):
+        """Test successful register"""
+        test_data = {
+            "redflag_id": 10,
+            "comment": "ewrtry",
+            "inter_location": "0.5, 32.5"
+        }
+        response = self.app.post('/api/v1/interventions', json=test_data)
+        self.assertEqual(response.status_code, 401)
+        assert json.loads(response.data)['msg'] == "Missing Authorization Header"
+
+
+    def test_edit_comment(self):
+        token = self.return_user_token()
+        test_data = {
+            "redflag_id": 1,
+            "comment": "wevlqnwcAOP",
+            "inter_location": "0.5, 32.5"
+        }
+        self.app.post('/api/v1/interventions', content_type="application/json", 
+        headers={"Authorization": "Bearer " + token}, json=test_data)
+        update_comment = {"comment": "asVUYI516789"}
+        response = self.app.patch('/api/v1/interventions/1/comment', 
+        headers={"Authorization": "Bearer " + token}, json=update_comment)
+        assert response.status_code == 200
+        assert json.loads(response.data)['redflag'][0]['message'] == "intervention comment updated"
+   

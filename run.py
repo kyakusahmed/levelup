927fffebe61f5b03e8ca6f802bed10ed648b4bf2
kyakulumbye ahmad <kyakusahmed@outlook.com>
2019-01-02 17:00:20 +0300

    add temporary challenge 3

--- run.py
+++ run.py
@@ -1,5 +1,10 @@
 from app import app
+from app.models.migration import Migration
 
 
 if __name__ == "__main__":
+    db = Migration()
+    db.create_tables()
+    db.drop_tables() 
+    db.create_tables()  
     app.run(debug=True, port=8080)

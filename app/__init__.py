17c5235d2fc2f64cee8872aed551a3559703a0db
kyakulumbye ahmad <kyakusahmed@outlook.com>
2019-01-02 17:23:07 +0300

    add temporary challenge 3

--- app/__init__.py
+++ app/__init__.py
@@ -3,11 +3,8 @@ from re import match
 
 app = Flask("__name__")
 
-from app.models import migration
+
 from app.models import db_conn
-from app.models import redflag
-from app.models import auth
-from app.models import interventions
 from app.views import interventions 
 from app.views import redflag
 from app.views import auth
--- run.py
+++ run.py
@@ -2,9 +2,11 @@ from app import app
 from app.models.migration import Migration
 
 
-if __name__ == "__main__":
-    db = Migration()
-    db.create_tables()
-    db.drop_tables() 
-    db.create_tables()  
+if __name__ == "__main__":  
     app.run(debug=True, port=8080)
+
+
+#     db = Migration()
+#     db.create_tables()
+#     db.drop_tables() 
+    # db.create_tables()    
